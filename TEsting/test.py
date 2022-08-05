d252d2610ffd98df91af40f640fb90a0a1482ff1
Emu51 <emu.emu750@gmail.com>
2022-08-05 16:49:38 +0600

    test1

---
+++ TEsting/test.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+x=[1,2,3]
+y=[4,5,6]
+
+plt.title('Graph 1')
+plt.xlabel('This is X axis')
+plt.ylabel('This is Y axis')
+
+plt.plot(x,y)
+plt.show()
